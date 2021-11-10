e232540a4414464dddcd283b846572fd622c0fa1
vytcka4 <vytautas.freelance@gmail.com>
2021-11-10 07:22:58 +0200

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const containers = document.querySelectorAll(".container");
+containers.addEventListener("click", function (x) {
+  x.preventDefault();
+  const newContent = document.createTextNode("X");
+  containers.appendChild(newContent);
+});
